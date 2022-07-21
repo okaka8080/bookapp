af521d16889818b4624bca4c95dd5008e4272188
okaka8080 <83401443+okaka8080@users.noreply.github.com>
2022-07-21 09:21:30 +0900

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,89 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Flutter Sample',
+      home: MyHomePage(title: 'Flutter Sample'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  List items = [];
+
+  Future<void> getData() async {
+    var response = await http.get(Uri.https(
+        'www.googleapis.com',
+        '/books/v1/volumes',
+        {'q': '{Flutter}', 'maxResults': '40', 'langRestrict': 'ja'}));
+
+    var jsonResponse = jsonDecode(response.body);
+
+    setState(() {
+      items = jsonResponse['items'];
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    getData();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter Sample'),
+      ),
+      body: ListView.builder(
+        itemCount: items.length,
+        itemBuilder: (BuildContext context, int index) {
+          return Card(
+            child: Column(
+              children: <Widget>[
+                ListTile(
+                  leading: Image.network(
+                    items[index]['volumeInfo']['imageLinks']['thumbnail'],
+                  ),
+                  title: Text(items[index]['volumeInfo']['title']),
+                  subtitle:
+                      (items[index]['volumeInfo']['publishedDate'] != null)
+                          ? Text(items[index]['volumeInfo']['publishedDate'])
+                          : const Text("not found"),
+                ),
+              ],
+            ),
+          );
+        },
+      ),
+    );
+  }
+
+  /*Widget ifText(int index) {
+    if (items[index]['volumeInfo']['publishedDate'] != null) {
+      return Text(items[index]['volumeInfo']['publishedDate']);
+    } else {
+      return const Text("not found");
+    }
+  }*/
+}
